0e0d82d71c71ef796871c0067bbfffa26029f696
Jose Portero <ytrop@hotmail.com>
2021-06-04 13:51:21 +0200

    Mejoras de vistas en las vistas

--- rutcom/resources/views/livewire/calendar.blade.php
+++ rutcom/resources/views/livewire/calendar.blade.php
@@ -1,3 +1,20 @@
+<x-slot name="header">
+    <h2 class="text-xl font-semibold leading-tight text-gray-800">
+        {{ __('Calendar') }}
+    </h2>
+</x-slot>
+
+<div class="py-12">
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
+            <div>
+                <div id='calendar-container' wire:ignore>
+                  <div id='calendar'></div>
+                </div>
+              </div>
+        </div>
+    </div>
+</div>
 <div>
     <div id='calendar-container' wire:ignore>
       <div id='calendar'></div>
